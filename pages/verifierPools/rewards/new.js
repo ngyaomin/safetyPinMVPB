1f0f879664db7bacd07417b6721a29909990e7e7
Yao Min <ngyaominsg@gmail.com>
2021-01-07 15:55:57 +0800

    Add new js for rewards

--- pages/verifierPools/rewards/new.js
+++ pages/verifierPools/rewards/new.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import { Form, Button, Message, Input } from 'semantic-ui-react';
-import InsurancePool from '../../../ethereum/insurancePool';
+import VerifierPool from '../../../ethereum/verifierPool';
 import web3 from '../../../ethereum/web3';
 import { Link, Router } from '../../../routes';
 import Layout from '../../../components/Layout';
 import Uploady from "@rpldy/uploady";
 import UploadButton from  "@rpldy/upload-button";
 
-class ClaimNew extends Component {
+class RewardNew extends Component {
   state = {
     value: '',
     description: '',
-    claimer: '',
+    staker: '',
     loading: false,
     errorMessage: ''
   };
@@ -25,20 +25,20 @@ class ClaimNew extends Component {
   onSubmit = async event => {
     event.preventDefault();
 
-    const insurancePool = InsurancePool(this.props.address);
-    const { description, value, claimer} = this.state;
+    const verifierPool = VerifierPool(this.props.address);
+    const { description, value, staker} = this.state;
 
     this.setState({ loading: true, errorMessage: '' });
 
     try {
       const accounts = await web3.eth.getAccounts();
-      await insurancePool.methods.createClaim(
+      await verifierPool.methods.createReward(
           description,
           web3.utils.toWei(value, 'ether'),
-          claimer
+          staker
       ).send({ from: accounts[0] });
 
-      Router.pushRoute(`/insurancepools/${this.props.address}/claims`);
+      Router.pushRoute(`/verifierpools/${this.props.address}/rewards`);
     } catch (err) {
       this.setState({ errorMessage: err.message })
     }
@@ -49,10 +49,10 @@ class ClaimNew extends Component {
   render() {
     return (
       <Layout>
-        <Link route={`/insurancepools/${this.props.address}/claims`}>
+        <Link route={`/verifierpools/${this.props.address}/rewards`}>
           <a>Back</a>
         </Link>
-        <h3>Create a new claim request</h3>
+        <h3>Create a new reward request</h3>
         <Uploady destination={{url: "https://fakeuploaddrive.fake"}}>
           <UploadButton/> Upload Supporting Files Here (for demo only real upload server cant afford )
         </Uploady>
@@ -78,20 +78,20 @@ class ClaimNew extends Component {
           </Form.Field>
 
           <Form.Field>
-            <label>Claimer</label>
+            <label>Staker</label>
             <Input
-              value={this.state.claimer}
+              value={this.state.staker}
               onChange={event =>
-                this.setState({ claimer: event.target.value  })}
+                this.setState({ staker: event.target.value  })}
             />
           </Form.Field>
 
           <Message error header="Something wrong" content={this.state.errorMessage} />
-          <Button primary loading={this.state.loading}>Request a new Claim</Button>
+          <Button primary loading={this.state.loading}>Request a new Reward</Button>
         </Form>
       </Layout>
     )
   }
 }
 
-export default ClaimNew;
+export default RewardNew;
