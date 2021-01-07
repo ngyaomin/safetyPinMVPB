33bf33d81f4f966ca3fd299cda9a4ed3dad4b80c
Yao Min <ngyaominsg@gmail.com>
2021-01-07 16:17:11 +0800

    update compile

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -6,8 +6,10 @@ const buildPath = path.resolve(__dirname, 'build');
 fs.removeSync(buildPath); // remove the build folder , so that content inside it only run once
 // ie we delete it and then run our own contents below
 
-const insurancePoolPath = path.resolve(__dirname, 'contracts', 'InsurancePool.sol', 'VerifierPool.sol');
+const insurancePoolPath = path.resolve(__dirname, 'contracts', 'InsurancePool.sol');
 const source = fs.readFileSync(insurancePoolPath, 'utf8' );
+const verifierPoolPath = path.resolve(__dirname, 'contracts', 'VerifierPool.sol');
+const source = fs.readFileSync(verifierPoolPath, 'utf8' );
 const output = solc.compile(source, 2).contracts; // there is only one source in this case one contracts
 
 // now we recreate the build folder
