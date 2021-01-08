ce9d79165fdc1ef84b8b1e4a1745f610c70e83de
Yao Min <ngyaominsg@gmail.com>
2021-01-09 03:17:16 +0800

    delete duplicate

--- ethereum/insurancePool.js
+++
@@ -1,9 +0,0 @@
-import web3 from './web3';
-import InsurancePool from './build/InsurancePool.json';
-
-export default address => {
-  return new web3.eth.Contract(
-    JSON.parse(InsurancePool.interface),
-    address
-  );
-};
